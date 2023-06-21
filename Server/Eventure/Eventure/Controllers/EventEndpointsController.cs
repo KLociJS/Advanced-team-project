6c9f6a27a2eef33a22deadd3a0169783fe1d1f52
BaloghBianka <biabalogh1282@gmail.com>
2023-06-21 11:02:47 +0200

    chore: Add event endpoints skeleton

--- Server/Eventure/Eventure/Controllers/EventEndpointsController.cs
+++ Server/Eventure/Eventure/Controllers/EventEndpointsController.cs
@@ -5,5 +5,39 @@ namespace Eventure.Controllers;
 [Route("[controller]")]
 public class EventEndpointsController: ControllerBase
 {
-    
+    [HttpPost()]
+    public IActionResult CreateEvent()
+    {
+        return Ok();
+    }
+
+    [HttpPut()]
+    public IActionResult UpdateEvent()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteEvent(int id)
+    {
+        return Ok(id);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetEventById(int id)
+    {
+        return Ok(id);
+    }
+
+    [HttpGet("random")]
+    public IActionResult GetRandomEvents()
+    {
+        return Ok();
+    }
+
+    [HttpGet("search")]
+    public IActionResult SearchEvent()
+    {
+        return Ok();
+    }
 }
