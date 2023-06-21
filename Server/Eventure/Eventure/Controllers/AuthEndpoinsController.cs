24be18ec198493b718d0a4aab6fdc0bc97015882
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-06-21 10:57:09 +0200

    chore: Add authentification endpoints skeleton

---
+++ Server/Eventure/Eventure/Controllers/AuthEndpoinsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Eventure.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthEndpointsController : ControllerBase
+{
+    //Login Endpoint
+    [HttpPost]
+    [Route("/api/login")]
+    public async Task<IActionResult> Login()
+    {
+        return Ok();
+    }
+
+    
+    //Logout Endpoint
+    [HttpPost]
+    [Route("/api/logout")]
+    public async Task<IActionResult> Logout()
+    {
+        return Ok();
+    }
+
+    
+    //Registration
+    [HttpPost]
+    [Route("/api/signup")]
+    public async Task<IActionResult> Signup()
+    {
+        return Ok("Registered successfully");
+    }
+}
