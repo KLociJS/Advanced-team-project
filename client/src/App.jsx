3727fe91d49d9bf3830973408094bd0281fab390
KLociJS <kisznerlorant21@gmail.com>
2023-07-19 09:13:11 +0200

    feat: Implement and style input fields

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom'
 
 import './Layout.css'
+import './Typography.css'
 
 import { Layout } from 'Layout'
 
---
+++ client/src/Components/index.jsx
@@ -0,0 +1,7 @@
+import Input from "./Input/Input";
+import DateInput from "./Input/DateInput.jsx/DateInput";
+
+export {
+    Input,
+    DateInput,
+}
