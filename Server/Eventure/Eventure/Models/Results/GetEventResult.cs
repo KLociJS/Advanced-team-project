6ba438c4507833b0dd0ccb7553d280b1c282b254
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-07-20 16:04:29 +0200

    chore: Add EventActionResult, GetEvenResult and endpoints for random, update, delete

---
+++ Server/Eventure/Eventure/Models/Results/GetEventResult.cs
@@ -0,0 +1,24 @@
+﻿using Eventure.Models.Entities;
+using Eventure.Models.ResponseDto;
+
+namespace Eventure.Models.Results;
+
+public class GetEventResult
+{
+    public bool Succeeded { get; set; }
+    public Response Response { get; set; }
+    public Event EventData { get; set; } 
+
+    public static GetEventResult Succeed(string message, Event eventData)
+    {
+        var response = new Response() { Message = message };
+        return new GetEventResult() { Succeeded = true, EventData = eventData, Response = response};
+    }
+
+    public static GetEventResult Failed(string message)
+    {
+        var response = new Response() { Message = message };
+        return new GetEventResult() { Succeeded = false, Response = response};
+
+    }
+}
--- Server/Eventure/Eventure/Services/IEventService.cs
+++ Server/Eventure/Eventure/Services/IEventService.cs
@@ -7,8 +7,10 @@ namespace Eventure.Services;
 public interface IEventService
 {
     Task<EventActionResult> CreateEventAsync(CreateEventDto createEventDto);
-    Task DeleteEvent(long id);
-    Task UpdateEvent(Event eventToUpdate);
+    Task<EventActionResult> DeleteEvent(string id);
+    Task<GetEventResult>  UpdateEvent(Event eventToUpdate);
     List<Location> GetLocation(string location);
     List<Category> GetCategory(string category);
+    Task<GetEventResult> GetEventByIdAsync(string id);
+    Task<GetEventResult> GetRandomEvent();
 }
