f0fc9db952de0624dfdb2db4330efe2b0b484c41
KLociJS <kisznerlorant21@gmail.com>
2023-07-30 12:35:15 +0200

    refactor: Crate reusable component from events

--- client/src/Pages/Events/Events.jsx
+++ client/src/Pages/Events/Events.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CardDisplayer, SearchForm } from 'Components'
 
 export default function Events() {
 
   const [isOpen,setIsOpen] = useState(false)
-  const eventsFetchUrl = 'https://localhost:7019/EventEndpoints/search'
+
+  const [events, setEvents] = useState([])
+
+  const searchUrl = 'https://localhost:7019/EventEndpoints/search'
+
+  useEffect(()=>{
+    fetch('https://localhost:7019/EventEndpoints')
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data.events);
+      setEvents(data.events)
+    })
+    .catch(console.log)
+  },[])
 
   return (
     <>
-      <SearchForm isOpen={isOpen} setIsOpen={setIsOpen} />
-      <CardDisplayer isOpen={isOpen} setIsOpen={setIsOpen} fetchUrl={eventsFetchUrl} />
+      <SearchForm isOpen={isOpen} setIsOpen={setIsOpen} url={searchUrl} setEvents={setEvents}/>
+      <CardDisplayer isOpen={isOpen} setIsOpen={setIsOpen} events={events} />
     </>
   )
 }
