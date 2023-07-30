a58b3a91f3cbe0a62fa2dcb43c53c9dd0cb88090
KLociJS <kisznerlorant21@gmail.com>
2023-07-30 12:30:17 +0200

    Refactor: Make card displayer a reusable component

--- client/src/Components/CardDisplayer/CardDisplayer.jsx
+++ client/src/Components/CardDisplayer/CardDisplayer.jsx
@@ -4,29 +4,19 @@ import './CardDisplayer.css'
 import { HiMiniMagnifyingGlass } from 'react-icons/hi2'
 import { EventPreviewCard } from 'Components'
 
-export default function CardDisplayer({ isOpen, setIsOpen, fetchUrl }) {
-  const [events, setEvents] = useState([])
-
-  useEffect(()=>{
-    fetch(fetchUrl)
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data.events);
-      setEvents(data.events)
-    })
-    .catch(console.log)
-  },[fetchUrl])
+export default function CardDisplayer({ isOpen, setIsOpen, events }) {
 
   return (
     <section className='card-container'>
       <div className='search-button-container'>
         <button className={`searchbar-toggle${isOpen ? ' hidden' : ''}`} onClick={()=>setIsOpen(b=>!b)}>
           Search
+        <HiMiniMagnifyingGlass className='searchbar-toggle-icon'/>
         </button>
       </div>
-        <HiMiniMagnifyingGlass className='searchbar-toggle-icon'/>
         {events.map(e=>(
-          <EventPreviewCard 
+          <EventPreviewCard
+            key={e.id}
             name={e.eventName}
             location={e.location.name}
             category={e.category.name}
