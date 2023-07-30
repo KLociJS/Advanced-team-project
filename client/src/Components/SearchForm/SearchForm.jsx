4c71549573d0bf087f32dcb1e2c4a3f591f3c7f6
KLociJS <kisznerlorant21@gmail.com>
2023-07-30 19:40:11 +0200

    refactor: Handle search params dinamically

--- client/src/Components/SearchForm/SearchForm.jsx
+++ client/src/Components/SearchForm/SearchForm.jsx
@@ -18,10 +18,8 @@ export default function SearchForm({isOpen, setIsOpen, url, setEvents}) {
 
     const [eventName, setEventName] = useState('')
     const [location, setLocation] = useState('')
-    const [locationId,setLocationId] = useState()
     const [distance, setDisctance] = useState('')
     const [category, setCategory] = useState('')
-    const [categoryId, setCategoryId] = useState()
     const [minPrice,setMinPrice] = useState('')
     const [maxPrice, setMaxPrice] = useState('')
     const [startDate, setStartDate] = useState('')
@@ -37,22 +35,34 @@ export default function SearchForm({isOpen, setIsOpen, url, setEvents}) {
 
         const startDateTime = startDate ? new Date(startDate).toISOString().replace(/\d{2}:\d{2}/g,startTime) : ''
         const endDateTime = endDate ? new Date(endDate).toISOString().replace(/\d{2}:\d{2}/g,endTime) : ''
+
+        const searchParamsDict = {
+            eventName ,
+            location,
+            category,
+            minPrice,
+            maxPrice,
+            startingDate: startDateTime,
+            endingDate: endDateTime
+        }
+
+        let searchParams = ''
+        Object.keys(searchParamsDict).forEach(key=>{
+            if(searchParamsDict[key]!==''){
+                if(searchParams === ''){
+                    searchParams = `?${key}=${searchParamsDict[key]}`
+                }else{
+                    searchParams += `&${key}=${searchParamsDict[key]}`
+                }
+            }
+        })
+
+        console.log(searchParams);
         
-
-        const eventNameParam = eventName ? `?eventName=${eventName}` : ''
-        const locationParam = location ? `&location=${location}` : ''
-        const categoryParam = category ? `&category=${category}` : ''
-        const minPriceParam = minPrice ? `&minPrice=${minPrice}` : ''
-        const maxPriceParam = maxPrice ? `&maxPrice=${maxPrice}` : ''
-        const startDateParam = startDate ? `?startingDate=${startDateTime}` : ''
-        const endDateParam = endDate ? `&endingDate=${endDateTime}` : ''
-
-        console.log(eventName,location,distance,category,minPrice,maxPrice,startDate, endDate)
-        fetch(`${url}${eventNameParam}${startDateParam}${locationParam}${categoryParam}${minPriceParam}${maxPriceParam}${endDateParam}`)
+        fetch(`${url}${searchParams}`)
         .then(res=>res.json())
         .then(data=>{
-        console.log(data.events);
-        setEvents(data.events)
+            setEvents(data.events)
         })
         .catch(console.log)
     }
@@ -66,25 +76,23 @@ export default function SearchForm({isOpen, setIsOpen, url, setEvents}) {
             <p className='mb-2 secondary-text'>Leaving a field empty doesnt filter by the given property.</p>
             <Input label='Event Name' inputValue={eventName} setInputValue={setEventName}/>
             <AutoCompleteInput 
-            label='Location'
-            inputValue={location}
-            setInputValue={setLocation}
-            setId={setLocationId}
-            url={locationsUrl}
+                label='Location'
+                inputValue={location}
+                setInputValue={setLocation}
+                url={locationsUrl}
             />
             <Input label='Max Distance From Location' type='number' inputValue={distance} setInputValue={setDisctance}/>
             <AutoCompleteInput 
-            label='Category'
-            inputValue={category}
-            setInputValue={setCategory}
-            setId={setCategoryId}
-            url={categoriesUrl}
+                label='Category'
+                inputValue={category}
+                setInputValue={setCategory}
+                url={categoriesUrl}
             />
             <DateInput label='Starting Date' dateValue={startDate} setDateValue={setStartDate} time={startTime} setTime={setStartTime}/>
             <DateInput label='Ending Date' dateValue={endDate} setDateValue={setEndDate} time={endTime} setTime={setEndTime}/>
             <div className='flex-container mb-1'>
-            <Input label='Min Price' type='number' inputValue={minPrice} setInputValue={setMinPrice} />
-            <Input label='Max Price' type='number' inputValue={maxPrice} setInputValue={setMaxPrice} />
+                <Input label='Min Price' type='number' inputValue={minPrice} setInputValue={setMinPrice} />
+                <Input label='Max Price' type='number' inputValue={maxPrice} setInputValue={setMaxPrice} />
             </div>
             <PrimaryButton text='Search' clickHandler={handleSearchSubmit}/>
             <SecondaryButton text='Cancel' clickHandler={()=>setIsOpen(false)} />
