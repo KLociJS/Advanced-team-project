2db91dbd75aa8b6eca54aaf685d78a434fbfeb9d
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-07-31 15:19:55 +0200

    chore: Add registration basic functionality

--- Server/Eventure/Eventure/Services/AuthService.cs
+++ Server/Eventure/Eventure/Services/AuthService.cs
@@ -1,6 +1,29 @@
+using Eventure.Models.Entities;
+using Eventure.Models.RequestDto;
+using Microsoft.AspNetCore.Identity;
+
 namespace Eventure.Services;
 
 public class AuthService:IAuthService
 {
-    
+    private readonly UserManager<User> _userManager;
+
+    public AuthService(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<bool> RegisterUser(RegisterUserDto registerUserDto)
+    {
+        var user = new User()
+        {
+            UserName = registerUserDto.Username,
+            Email = registerUserDto.Email
+        };
+        var registerResult = await _userManager.CreateAsync(user, registerUserDto.Password);
+       
+            return registerResult.Succeeded;
+        
+        
+    }
 }
--- client/src/Pages/Signup/Signup.jsx
+++ client/src/Pages/Signup/Signup.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {  
+  Input, PrimaryButton,
+ } from 'Components'
+
 
 export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+    
+ const handleSubmit = () => {
+  const userData = {
+    username, 
+    email,
+    password
+  };
+  fetch('https://localhost:7019/api/signup', {
+method: "POST",
+headers: {
+  "Content-Type": "application/json" 
+},
+body: JSON.stringify(userData)
+  })
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+  
+ }
+  
   return (
-    <div>Signup</div>
-  )
-}
+    <div>
+      
+        <Input label={"Username"} inputValue={username} setInputValue={setUsername}/>
+        <Input label={"Email"} inputValue={email} setInputValue={setEmail}/>
+        <Input label={"Password"} type={"password"} inputValue={password} setInputValue={setPassword}/>
+        <PrimaryButton text={"Register"} clickHandler={handleSubmit} />
+    </div>    
+  );
+  
+  }
