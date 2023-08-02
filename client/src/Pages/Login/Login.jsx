e483135a5f5c4eb931aa6921393e744a85969ff2
KLociJS <kisznerlorant21@gmail.com>
2023-08-02 06:46:50 +0200

    chore: Add auth context

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,8 +1,10 @@
 import useAuth from 'Hooks/useAuth'
 import React, { useState } from 'react'
-import {  
+import useAuth from 'Hooks/useAuth'
+
+import {
   Input, PrimaryButton,
- } from 'Components'
+} from 'Components'
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
@@ -10,42 +12,50 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const { setUser } = useAuth()
 
   const handleSubmit = () => {
+
     const userData = {
-      userName, 
+      userName,
       password
     };
-    console.log(userData);
-    fetch('https://localhost:7019/api/login', {
-  method: "POST",
-  credentials:"include",
-  headers: {
-    "Content-Type": "application/json" 
-  },
-  body: JSON.stringify(userData)
-  })
-  .then(res => {
-    if(res.ok){
-       return res.json()
-    }else{
-      throw res;
-    }
-   
-  })
-.then(data => {
-  console.log(data)
-  navigate("/")
-})
 
+<<<<<<< HEAD
 }
 const { setUser } = useAuth()
+=======
+    console.log(userData);
+
+    fetch('https://localhost:7019/api/login', {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw res;
+      }
+
+    })
+    .then(data => {
+      console.log(data)
+      navigate("/")
+    })
+  }
+
+>>>>>>> dc82f19 (chore: Add auth context)
   return (
     <div>
-    <Input label={"Username"} inputValue={userName} setInputValue={setUserName}/>
-    <Input label={"Password"} type={"password"} inputValue={password} setInputValue={setPassword}/>
-    <PrimaryButton text={"Log In"} clickHandler={handleSubmit} />
-    {error ? <p>{error}</p> : null}
-</div>    
+      <Input label={"Username"} inputValue={userName} setInputValue={setUserName} />
+      <Input label={"Password"} type={"password"} inputValue={password} setInputValue={setPassword} />
+      <PrimaryButton text={"Log In"} clickHandler={handleSubmit} />
+      {error ? <p>{error}</p> : null}
+    </div>
   )
 }
